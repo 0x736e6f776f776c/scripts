3f12eafa6201b58a837270354a13c271b03a4eff
Vladimir <66011441+0x736e6f776f776c@users.noreply.github.com>
2022-06-25 18:55:39 +0200

    Project transferred to a new repository
    Project transferred to a new repository for privacy-related reasons, I originally started this project in 2020

---
+++ Python Dependency Installer/Python Dependency Installer (only installer auto-deletion).bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d %~dp0
+echo Installing dependencies...
+echo.
+title Dependency Installer
+@pip install -r requirements.txt
+echo.
+echo Dependencies Installed!
+pause
+(goto) 2>nul & del "%~f0"
+exit
+
+:: This batch file installs the Python dependencies in requirements.txt
+:: Written by TechSnowOwl
